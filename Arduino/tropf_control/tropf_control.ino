75ee03afd8f0fb022d11c8dbe3cabd2cd5be5f3d
wakalixes <albert.frisch@gmail.com>
2013-09-09 00:15:57 +0200

    after installation, vertical time printing added

--- Arduino/tropf_control/tropf_control.ino
+++ Arduino/tropf_control/tropf_control.ino
@@ -68,6 +68,7 @@
 #define MAXNUMPUMPS    16
 #define MAXNUMTANKS    5
 #define DEBUGSTATE     0xFFFF
+#define DEBUGINTERVAL  2000
 #define TESTENABLE     0
 #define TESTTIMEDIFF   1000
 
@@ -86,23 +87,26 @@ const char CMDGETCONFIG[] =     "getconfig";
 const char CMDCHGINTERVAL[] =   "chginterval";
 const char CMDCHGONTIME[] =     "chgontime";
 const char CMDCHGPUMPFACT[] =   "chgpumpfact";
-const char CMDRESETPUMPFACT[] = "resetpumpfact";
-const char CMDPRINTTIME[] =     "printtime";
-const char CMDPRINTTEXT[] =     "printtext";
-const char CMDMIRRORTEXT[] =    "mirrortext";
-const char CMDAUTOPRINT[] =     "autoprint";
-const char CMDDEBUGPUMP[] =     "debugpump";
+const char CMDRESETPUMPFACT[] = "rpfact";
+const char CMDPRINTTIME[] =     "ptime";
+const char CMDPRINTTEXT[] =     "ptext";
+const char CMDMIRRORTEXT[] =    "mtext";
+const char CMDAUTOPRINT[] =     "aprint";
+const char CMDDEBUGPUMP[] =     "dpump";
+const char CMDDEBUGTIMING[] =   "dtiming";
 const char CMDRESETCONFIG[] =   "resetconfig";
 const char CMDSTARTOPERAT[] =   "startop";
 const char CMDSTOPOPERAT[] =    "stopop";
-const char CMDAUTOOPERAT[] =    "autoop";
+const char CMDAUTOOPERAT[] =    "aop";
 const char CMDGETSTATUS[] =     "getstatus";
 const char CMDGETLEVELS[] =     "getlevels";
-const char CMDCHGPLEVELMIN[] =  "chgplevelmin";
-const char CMDCHGPLEVELMAX[] =  "chgplevelmax";
+const char CMDCHGPLEVELMIN[] =  "chgplmin";
+const char CMDCHGPLEVELMAX[] =  "chgplmax";
 const char CMDGETBATV[] =       "getbat";
-const char CMDAUTORASPI[] =     "autoswitchraspi";
-const char CMDSWITCHRASPI[] =   "switchraspi";
+const char CMDAUTORASPI[] =     "aswpi";
+const char CMDSWITCHRASPI[] =   "swpi";
+const char CMDPRINTVERT[] =     "pvert";
+const char CMDPRINTTWOCHAR[] =  "ptwo";
 
 #define RESETOPERATION   1
 #define RESETAUTOOPERAT  0
@@ -112,12 +116,17 @@ const char CMDSWITCHRASPI[] =   "switchraspi";
 #define RESETAUTOPRINT   0
 #define RESETAUTOTIME    60
 #define RESETDEBUG       0
+#define RESETDEBUGTIMING 0
 #define RESETMIRRORTEXT  0
 #define RESETLEVELMIN    0
 #define RESETLEVELMAX    1023
 #define RESETSWTICHRASPI 1
+#define RESETPRINTVERT   0
+#define RESETPRINTTWO    0
 
 #define CHARMAXROWTIME   5
+#define CHARMAXROWBIG    7
+#define CHARROWSEMPTY    1
 #define CHARMAXROWTEXT   5
 #define CHARTEXTROWSPACE 1
 #define LOADCONFIGEEPROM 1
@@ -134,10 +143,12 @@ String printTextString = "";
 boolean stringComplete = false;
 boolean commandAck = false;
 unsigned long shiftSwitchTime;
+unsigned long debugSwitchTime;
 unsigned int predebugstate;
 unsigned int debugpumpstate;
 unsigned long raspiTrigTime;
 unsigned long oldtesttime;
+unsigned int statusoldhour;
 
 struct flagsStruct {
   boolean sendconfig;
@@ -159,6 +170,7 @@ struct flagsStruct {
   boolean raspioff;
   boolean opon;
   boolean opoff;
+  boolean debugpumpon;
 } flags;
 
 struct configStruct {
@@ -172,8 +184,11 @@ struct configStruct {
   boolean autooperatenable;
   boolean autoswitchraspi;
   boolean debugpumpenable;
+  boolean debugtiming;
   unsigned int autoprinttime;
   boolean mirrortext;
+  boolean printvert;
+  boolean printtwochar;
   time_t timecode;
 } config;
 
@@ -229,17 +244,11 @@ void loop() {
   raspiFlags();
   measLevels();
   measBatVoltage();
+  checkSendStatus();
   if (config.debugpumpenable) {
-    shiftWrite(debugpumpstate);
+    checkDebug();
   } else {
-    if (config.autooperatenable) checkOperatingHours();
-    if (config.isoperating) {
-      checkAutoPrint();
-      printFlags();
-      printTiming();
-    } else {
-      shiftWrite(0);
-    }
+    checkPrinting();
   }
   if (TESTENABLE) {
     if ((millis()-oldtesttime)>TESTTIMEDIFF) {
@@ -269,9 +278,12 @@ void resetVariables() {
     config.levelmax[i] = RESETLEVELMAX;
   }
   config.debugpumpenable = RESETDEBUG;
+  config.debugtiming = RESETDEBUGTIMING;
   config.autoprintenable = RESETAUTOPRINT;
   config.autoprinttime = RESETAUTOTIME;
   config.mirrortext = RESETMIRRORTEXT;
+  config.printvert = RESETPRINTVERT;
+  config.printtwochar = RESETPRINTTWO;
   config.timecode = now();
   writeConfigEEPROM();
 }
@@ -288,6 +300,38 @@ void initRTC() {
   else
     Serial.println(F("ERROR: RTC is NOT connected!"));
   Serial.println();
+}
+
+void checkDebug() {
+  if (config.debugtiming) {
+    if (flags.debugpumpon) {
+      if ((millis()-debugSwitchTime)>=config.pumpontime) {
+        flags.debugpumpon = false;
+        shiftWrite(0);
+        Serial.println(F("debugpump switch off"));
+      }
+    } else {
+      if ((millis()-debugSwitchTime)>=DEBUGINTERVAL) {
+        flags.debugpumpon = true;
+        debugSwitchTime = millis();
+        shiftWrite(debugpumpstate);
+        Serial.println(F("debugpump switch on"));
+      }
+    }
+  } else {
+    shiftWrite(debugpumpstate);
+  }
+}
+
+void checkPrinting() {
+  if (config.autooperatenable) checkOperatingHours();
+  if (config.isoperating) {
+    checkAutoPrint();
+    printFlags();
+    printTiming();
+  } else {
+    shiftWrite(0);
+  }  
 }
 
 void checkAutoPrint() {
@@ -408,22 +452,49 @@ void switchRaspiOff() {
   statusVal.israspion = false;
 }
 
+void checkSendStatus() {
+  if (hour()!=statusoldhour) {
+    statusoldhour = hour();
+    flags.sendstatus = 1;
+  }
+}
+
 void printFlags() {
   if (flags.printtime) {
     if (flags.printnextrow) {
-      flags.printnextrow = 0;
-      unsigned int shiftBits = getPrintTimeRow(printTimeData.rowIdx,printTimeData.hour,printTimeData.minute);
-      shiftInitWrite(shiftBits);
-      if (printTimeData.rowIdx==0) {
-        Serial.println(F("printing start"));
-        if (SENDWEBCAMTRIG==1) Serial.println(F("trigger webcam"));  
-      }
-      serialSendPrintTime(shiftBits);
-      if (printTimeData.rowIdx++>=CHARMAXROWTIME-1) {
-        flags.printtime = 0;
-        printTimeData.rowIdx = 0;
-        Serial.println(F("printing done"));
-        Serial.println();
+      if (config.printvert) {
+        flags.printnextrow = 0;
+        unsigned int shiftBits = getPrintTimeRowBig(printTimeData.rowIdx,printTimeData.hour,printTimeData.minute);
+        shiftInitWrite(shiftBits);
+        if (printTimeData.rowIdx==0) {
+          Serial.println(F("printing start"));
+          if (SENDWEBCAMTRIG==1) Serial.println(F("trigger webcam"));  
+        }
+        serialSendPrintTime(shiftBits);
+        int maxRow;
+        if (config.printtwochar) maxRow = (3*CHARMAXROWBIG+2*CHARROWSEMPTY)-1;
+        else maxRow = (5*CHARMAXROWBIG+4*CHARROWSEMPTY)-1;
+        if (printTimeData.rowIdx++>=maxRow) {
+          flags.printtime = 0;
+          printTimeData.rowIdx = 0;
+          Serial.println(F("printing done"));
+          Serial.println();
+        }
+      } else {
+        flags.printnextrow = 0;
+        unsigned int shiftBits = getPrintTimeRow(printTimeData.rowIdx,printTimeData.hour,printTimeData.minute);
+        shiftInitWrite(shiftBits);
+        if (printTimeData.rowIdx==0) {
+          Serial.println(F("printing start"));
+          if (SENDWEBCAMTRIG==1) Serial.println(F("trigger webcam"));  
+        }
+        serialSendPrintTime(shiftBits);
+        if (printTimeData.rowIdx++>=CHARMAXROWTIME-1) {
+          flags.printtime = 0;
+          printTimeData.rowIdx = 0;
+          Serial.println(F("printing done"));
+          Serial.println();
+        }
       }
     }
   }
@@ -461,7 +532,7 @@ void printTiming() {
         shiftWrite(0);
         flags.pumpson = 0;
         flags.pumpsoff = 1;
-        shiftSwitchTime = shiftSwitchTime+config.pumpontime;
+        if (!config.printvert) shiftSwitchTime = shiftSwitchTime+config.pumpontime;
         flags.printcheckontime = false;
       } else {
         if (printOnData.curShiftBits != printOnData.oldShiftBits) {
@@ -472,9 +543,16 @@ void printTiming() {
     }
   }
   if ((!flags.pumpson)&&(flags.pumpsoff)) {
-    if ((millis()-shiftSwitchTime) >= config.printinterval-config.pumpontime) {
-      flags.pumpsoff = 0;
-      flags.printnextrow = 1;
+    if (config.printvert) {
+      if ((millis()-shiftSwitchTime) >= config.printinterval) {
+        flags.pumpsoff = 0;
+        flags.printnextrow = 1;
+      }
+    } else {
+      if ((millis()-shiftSwitchTime) >= config.printinterval-config.pumpontime) {
+        flags.pumpsoff = 0;
+        flags.printnextrow = 1;
+      }
     }
   }  
 }
@@ -512,8 +590,60 @@ unsigned int getPrintTimeRow(unsigned char i, unsigned char printhour, unsigned 
   return (shiftBitsHour<<7)|(shiftBitsPoint<<6)|(shiftBitsMinute<<0);
 }
 
+unsigned int getPrintTimeRowBig(unsigned char i, unsigned char printhour, unsigned char printminute) {
+  int charnum;
+  int rownum;
+  unsigned char hourtenths = printhour/10;
+  unsigned char hourones = printhour%10;
+  unsigned char minutetenths = printminute/10;
+  unsigned char minuteones = printminute%10;
+  if (config.printtwochar) {
+    if (i>3*(CHARMAXROWBIG+CHARROWSEMPTY)) i = 3*(CHARMAXROWBIG+CHARROWSEMPTY);
+  } else {
+    if (i>5*(CHARMAXROWBIG+CHARROWSEMPTY)) i = 5*(CHARMAXROWBIG+CHARROWSEMPTY);
+  }
+  charnum = i/(CHARMAXROWBIG+CHARROWSEMPTY);
+  rownum = (i-charnum*(CHARMAXROWBIG+CHARROWSEMPTY));
+  int printdigit;
+  int printdigitleft;
+  int printdigitright;
+  int shiftbitsbig = 0;
+  if (config.printtwochar) {
+    switch (charnum) {
+      case 0: printdigitleft = hourtenths;
+              printdigitright = hourones;
+              break;
+      case 1: printdigitleft = 11;
+              printdigitright = 11;
+              break;
+      case 2: printdigitleft = minutetenths;
+              printdigitright = minuteones;
+              break;
+    }
+    if (rownum>=CHARMAXROWBIG) shiftbitsbig = 0;
+    else shiftbitsbig = (getDigitBitsBig(printdigitleft,rownum)<<8)|(getDigitBitsBig(printdigitright,rownum)<<0);
+  } else {
+    switch (charnum) {
+      case 0: printdigit = hourtenths; break;
+      case 1: printdigit = hourones; break;
+      case 2: printdigit = 10; break;
+      case 3: printdigit = minutetenths; break;
+      case 4: printdigit = minuteones; break;
+    }
+    if (rownum>=CHARMAXROWBIG) shiftbitsbig = 0;
+    else shiftbitsbig = (getDigitBitsBig(printdigit,rownum)<<7);
+  }
+  return shiftbitsbig;
+}
+
 unsigned char getDigitBits(unsigned char digit, unsigned char row) {
-  return (digitMap[digit][row][0]<<2)|(digitMap[digit][row][1]<<1)|(digitMap[digit][row][2]<<0);
+  return (pgm_read_byte(&(digitMap[digit][row][0]))<<2)|(pgm_read_byte(&(digitMap[digit][row][1]))<<1)|(pgm_read_byte(&(digitMap[digit][row][2]))<<0);
+  //return (digitMap[digit][row][0]<<2)|(digitMap[digit][row][1]<<1)|(digitMap[digit][row][2]<<0);
+}
+
+unsigned char getDigitBitsBig(unsigned char digit, unsigned char row) {
+  return (pgm_read_byte(&(digitMapBig[digit][row][0]))<<4)|(pgm_read_byte(&(digitMapBig[digit][row][1]))<<3)|(pgm_read_byte(&(digitMapBig[digit][row][2]))<<2)|(pgm_read_byte(&(digitMapBig[digit][row][3]))<<1)|(pgm_read_byte(&(digitMapBig[digit][row][4]))<<0);
+  //return (digitMapBig[digit][row][0]<<4)|(digitMapBig[digit][row][1]<<3)|(digitMapBig[digit][row][2]<<2)|(digitMapBig[digit][row][3]<<1)|(digitMapBig[digit][row][4]<<0);
 }
 
 unsigned int getPrintTextRow(unsigned int charIdx, unsigned int colIdx) {
@@ -528,7 +658,7 @@ unsigned int getCharBits(unsigned char printChar, unsigned char column) {
   unsigned char dataBits;
   dataBits = pgm_read_byte(&(charSet[printChar-0x20][column]));
   if (config.mirrortext) dataBits = reverseBitOrder(dataBits);
-  return (dataBits<<8);
+  return (dataBits<<7);
 }
 
 unsigned char reverseBitOrder(unsigned char input) {
@@ -672,6 +802,9 @@ void serialSendConfig() {
   Serial.print(F("debugpump: "));
   if (config.debugpumpenable) Serial.println("on");
   else Serial.println("off");
+  Serial.print(F("debugtiming: "));
+  if (config.debugtiming) Serial.println("on");
+  else Serial.println("off");
   Serial.print(F("autoprint (s): "));
   if (config.autoprintenable) Serial.print("on");
   else Serial.print("off");
@@ -679,6 +812,14 @@ void serialSendConfig() {
   Serial.println(config.autoprinttime);
   Serial.print(F("mirrortext: "));
   if (config.mirrortext) Serial.print("on");
+  else Serial.print("off");
+  Serial.println();
+  Serial.print(F("printvert: "));
+  if (config.printvert) Serial.print("on");
+  else Serial.print("off");
+  Serial.println();
+  Serial.print(F("printtwochar: "));
+  if (config.printtwochar) Serial.print("on");
   else Serial.print("off");
   Serial.println();
   Serial.print(F("timecode (s): "));
@@ -984,6 +1125,19 @@ void serialCommands() {
         commandAck = true;  
       }
     }
+    if (inputString.startsWith(CMDDEBUGTIMING)) {
+      commandString = inputString.substring(inputString.indexOf(CMDSEP)+1);
+      if (commandString.startsWith(CMDHIGH)) {
+        config.debugtiming = true;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+      if (commandString.startsWith(CMDLOW)) {
+        config.debugtiming = false;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+    }
     if (inputString.startsWith(CMDRESETCONFIG)) {
       resetVariables();
       commandAck = true;  
@@ -997,6 +1151,32 @@ void serialCommands() {
       }
       if (commandString.startsWith(CMDLOW)) {
         config.mirrortext = false;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+    }
+    if (inputString.startsWith(CMDPRINTVERT)) {
+      commandString = inputString.substring(inputString.indexOf(CMDSEP)+1);
+      if (commandString.startsWith(CMDHIGH)) {
+        config.printvert = true;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+      if (commandString.startsWith(CMDLOW)) {
+        config.printvert = false;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+    }
+    if (inputString.startsWith(CMDPRINTTWOCHAR)) {
+      commandString = inputString.substring(inputString.indexOf(CMDSEP)+1);
+      if (commandString.startsWith(CMDHIGH)) {
+        config.printtwochar = true;
+        writeConfigEEPROM();
+        commandAck = true;
+      }
+      if (commandString.startsWith(CMDLOW)) {
+        config.printtwochar = false;
         writeConfigEEPROM();
         commandAck = true;
       }
